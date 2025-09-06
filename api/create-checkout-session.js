d9dc25b5e4b2c5a8d437113268d81f26cf09d3c7
Manateek1 <dillon.nagar@gmail.com>
2025-09-06 02:13:01 -0700

    Add error logging to checkout session

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
+      payment_method_types: ["card"],
       line_items: [
         {
           price: process.env.PRICE_ID,
